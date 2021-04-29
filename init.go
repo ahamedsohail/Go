8d7c20ca25353652a5794aaa9fb1e91906fc79d1
ahamedsohail <soahamed.2020.development@gmail.com>
2021-04-29 16:39:47 +0530

    git add and commit test

--- init.go
+++ init.go
@@ -5,7 +5,10 @@ import "fmt"
 func main(){
 	fmt.Println("Hello, World!")
 	fmt.Println("Enter Name")
+	fmt.Println("Enter your age")
 	var name string
+	var age int
 	fmt.Scanln(&name)
-	fmt.Printf("Your name is %s",name)
+	fmt.Scanln(&age)
+	fmt.Printf("Name : %s  Age : %d",name,age)
 }
